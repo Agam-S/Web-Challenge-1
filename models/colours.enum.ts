6e4de72003fe7147ee449ff2bde9025659290e25
Agam-S <singh.agam0009@gmail.com>
2021-08-19 09:07:28 +1000

    models done

--- models/colours.enum.ts
+++ models/colours.enum.ts
@@ -5,4 +5,27 @@ export enum Colours {
   Green
 }
 
-// TODO: implement a ColoursHelper class as shown in BodyParts
+// ✅ TODO: implement a ColoursHelper class as shown in BodyParts
+export class ColoursHelper {
+  static colours: Colours[] = [
+    Colours.Red,
+    Colours.Blue,
+    Colours.Yellow,
+    Colours.Green
+  ];
+
+  constructor() {}
+
+  static get(key: string): Colours {
+    switch (key) {
+      case 'Red':
+        return Colours.Red;
+      case 'Blue':
+        return Colours.Blue;
+      case 'Yellow':
+        return Colours.Yellow;
+      case 'Green':
+        return Colours.Green;
+    }
+  }
+}
--- models/spin.ts
+++ models/spin.ts
@@ -1,5 +1,5 @@
-import { BodyParts } from "./bodyParts.enum";
-import { Colours } from "./colours.enum";
+import { BodyParts } from './bodyParts.enum';
+import { Colours } from './colours.enum';
 
 export interface ISpin {
   colour: Colours;
@@ -8,6 +8,20 @@ export interface ISpin {
 
 // TODO: create a SpinRecord class which implements ISpin and adds a new attribute num:number
 export class SpinRecord implements ISpin {
+  player: string;
+  colour: Colours;
+  bodyPart: BodyParts;
+  num: Number;
 
-
+  constructor(
+    player: string,
+    colour: Colours,
+    bodypart: BodyParts,
+    num: Number
+  ) {
+    this.player = player;
+    this.colour = colour;
+    this.bodyPart = bodypart;
+    this.num = num;
+  }
 }
